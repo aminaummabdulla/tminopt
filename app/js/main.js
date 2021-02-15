90ab73b112b12cc4c7d00066bdedc196b5a1d582
aminaummabdulla <tminopt@gmail.com>
2021-02-15 22:44:17 +0200

    create adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -28,18 +28,48 @@ $(function(){
       });
 
       $('.new__slider-inner').slick({
-        infinite: true,
         slidesToShow: 4,
         slidesToScroll: 4,
         speed: 3000, 
         prevArrow:'<button class="slick__new-arrow slick__new-prev"></button>',
         nextArrow:'<button class="slick__new-arrow slick__new-next"></button>',
         autoplay: true,
-        autoplaySpeed: 2000
+        autoplaySpeed: 2000,
+        responsive: [
+          {
+            breakpoint: 1060,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3, 
+            }
+          },
+          {
+            breakpoint: 870,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2, 
+            }
+          },
+          {
+            breakpoint: 760,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1, 
+            }
+          },
+        ]
 
       });
-   
+      
      
+      $('.header__bottom-btn').on('click', function(){
+        $('.nav__list').slideToggle();
+        });
+        
+        $('.catalog-mobile').on('click', function(){
+          $('.catalog-mobile-bottom').slideToggle();
+          });
+          
       var mixer = mixitup('.popular__items');
 
        
